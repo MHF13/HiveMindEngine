7137574ef7f94e0a334bb59b7f7879d7813cadc4
Makinilla-maker <60987205+Makinilla-maker@users.noreply.github.com>
2021-09-21 20:21:38 +0200

    Homework done
    Co-Authored-By: Mario HerFer <41444696+MHF13@users.noreply.github.com>

--- HiveMind/ModuleSceneIntro.h
+++ HiveMind/ModuleSceneIntro.h
@@ -19,8 +19,9 @@ public:
 
 	bool done = false;
 
-	bool show_demo_window = true;
-	bool show_another_window = false;
+	bool showGuiDemo = false;
+	bool showSmallExample = true;
+	bool closeWindow = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 public:
