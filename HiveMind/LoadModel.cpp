d108b6dd5f3b3e32668c63e39adc8c630551cbb2
Makinilla-maker <60987205+Makinilla-maker@users.noreply.github.com>
2021-10-18 20:49:50 +0200

    Tring things

--- HiveMind/LoadModel.cpp
+++ HiveMind/LoadModel.cpp
@@ -36,17 +36,37 @@ bool LoadModel::Start()
 		aiReleaseImport(scene);
 		LOG("Load %s", "Assets/Models/fish.fbx");
 
+		/////////////////////////////////////////////////
+		model Mod1;
+		aiMesh AAAA = **scene->mMeshes;
+		aiMesh AAA = **scene->mMeshes;
+		Mod1.num_vertex = AAAA.mNumVertices;
+		Mod1.vertex = new float[Mod1.num_vertex * 3];
+		memcpy(Mod1.vertex, AAAA.mVertices, sizeof(float) * Mod1.num_vertex * 3);
+		LOG("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nNew mesh with %d vertices", Mod1.num_vertex);
+
+		if (AAAA.HasFaces())
+		{
+			Mod1.num_index = AAAA.mNumFaces * 3;
+			Mod1.index = new uint[Mod1.num_index]; // assume each face is a triangle
+			for (uint i = 0; i < AAAA.mNumFaces; ++i)
+			{
+				if (AAAA.mFaces[i].mNumIndices != 3)
+				{
+					LOG("WARNING, geometry face with != 3 indices!");
+				}
+				else
+					memcpy(&Mod1.index[i * 3], AAA.mFaces[i].mIndices, 3 * sizeof(uint));
+			}
+		}
+		////////////////////////////////////////////////////////
+
 	}
 	else
 		LOG("Error loading scene %s", "Assets/Models/fish.fbx");
 	
 	//Dentro del for hara esto con cada ciclo (meter esto en una funcion, void aiMesh)
-	model Mod1;
-	aiMesh AAAA = **scene->mMeshes;
-	Mod1.num_vertex = AAAA.mNumVertices;
-	Mod1.vertex = new float[Mod1.num_vertex * 3];
-	memcpy(Mod1.vertex, AAAA.mVertices, sizeof(float) * Mod1.num_vertex * 3);
-	LOG("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nNew mesh with %d vertices", Mod1.num_vertex);
+	
 	
 	return ret;
 }
